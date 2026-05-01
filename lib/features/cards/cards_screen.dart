854c75a20785ffc75a1eaf8c5f1a72040acd4978
RonaldB03 <ronald.bakker.5@gmail.com>
2026-05-01 14:14:46 +0200

    2

--- lib/features/cards/cards_screen.dart
+++ lib/features/cards/cards_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'add_card_screen.dart';
+import 'card_detail_screen.dart';
+
 class CardsScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> items;
+  final Function(String id) onDelete;
+  final Function(String id) onToggleFavorite;
   final VoidCallback onAdd;
 
   const CardsScreen({
     super.key,
+    required this.items,
+    required this.onDelete,
+    required this.onToggleFavorite,
     required this.onAdd,
   });
 
-  static const List<_DefaultCard> defaultCards = [
+  static const defaultCards = [
     _DefaultCard(
       name: 'Albert Heijn',
       logoAsset: 'assets/logos/albert_heijn.png',
@@ -22,7 +31,7 @@ class CardsScreen extends StatelessWidget {
     _DefaultCard(
       name: 'Jumbo',
       logoAsset: 'assets/logos/jumbo.png',
-      color: Color(0xFFF6C400),
+      color: Color(0xFFFFC400),
     ),
     _DefaultCard(
       name: 'HEMA',
@@ -34,7 +43,7 @@ class CardsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF4F4F5),
+      backgroundColor: const Color(0xFFF4F4F6),
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -42,102 +51,148 @@ class CardsScreen extends StatelessWidget {
         title: const Text(
           'Klantenkaarten',
           style: TextStyle(
-            color: Color(0xFF3A3A3C),
-            fontWeight: FontWeight.w700,
+            color: Color(0xFF333333),
+            fontWeight: FontWeight.w800,
           ),
         ),
         actions: [
           IconButton(
+            icon: const Icon(Icons.add, color: Color(0xFFD51B46), size: 32),
             onPressed: onAdd,
-            icon: const Icon(Icons.add, color: Color(0xFFD51B46)),
           ),
         ],
       ),
       body: ListView(
-        padding: const EdgeInsets.fromLTRB(24, 36, 24, 24),
+        padding: const EdgeInsets.fromLTRB(24, 36, 24, 28),
         children: [
-          const Text(
-            'Nog geen klantenkaarten toegevoegd',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 32,
-              height: 1.15,
-              fontWeight: FontWeight.w500,
-              color: Color(0xFF444446),
+          if (items.isEmpty) ...[
+            const Text(
+              'Nog geen klantenkaarten toegevoegd',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 34,
+                height: 1.12,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF444446),
+              ),
             ),
-          ),
-          const SizedBox(height: 12),
-          const Text(
-            'Selecteer een kaart die je wilt toevoegen of tik op + voor meer opties',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 17,
-              height: 1.3,
-              color: Color(0xFF555557),
+            const SizedBox(height: 14),
+            const Text(
+              'Selecteer een winkel of tik op + om direct te scannen',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                height: 1.3,
+                color: Color(0xFF555557),
+              ),
             ),
-          ),
-          const SizedBox(height: 42),
+            const SizedBox(height: 48),
+            _DefaultCardsGrid(),
+          ] else ...[
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: items.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                childAspectRatio: 1.55,
+              ),
+              itemBuilder: (context, index) {
+                final item = items[index];
 
-          GridView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: defaultCards.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 14,
-              crossAxisSpacing: 14,
-              childAspectRatio: 1.65,
-            ),
-            itemBuilder: (context, index) {
-              final card = defaultCards[index];
-
-              return InkWell(
-                borderRadius: BorderRadius.circular(14),
-                onTap: () {
-                  // Later: open scanner met gekozen winkel
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: card.color,
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  padding: const EdgeInsets.all(18),
-                  child: Center(
-                    child: Image.asset(
-                      card.logoAsset,
-                      fit: BoxFit.contain,
+                return InkWell(
+                  borderRadius: BorderRadius.circular(18),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => CardDetailScreen(item: item),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.06),
+                          blurRadius: 12,
+                          offset: const Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        item['name']?.toString() ?? 'Kaart',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xFF333333),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-
-          const SizedBox(height: 48),
-
-          Row(
-            children: [
-              Expanded(
-                child: _ActionCard(
-                  icon: Icons.photo_camera_outlined,
-                  title: 'Kaart scannen',
-                  subtitle: 'Foto maken van fysieke kaart',
-                  onTap: onAdd,
-                ),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: _ActionCard(
-                  icon: Icons.login_outlined,
-                  title: 'Importeren',
-                  subtitle: 'Overzetten vanuit een andere app',
-                  onTap: () {},
-                ),
-              ),
-            ],
-          ),
+                );
+              },
+            ),
+          ],
         ],
       ),
+    );
+  }
+}
+
+class _DefaultCardsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: CardsScreen.defaultCards.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        childAspectRatio: 1.65,
+      ),
+      itemBuilder: (context, index) {
+        final card = CardsScreen.defaultCards[index];
+
+        return InkWell(
+          borderRadius: BorderRadius.circular(18),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => AddCardScreen(
+                  initialType: 'Pasje',
+                  initialName: card.name,
+                ),
+              ),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: card.color,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: Center(
+              child: Image.asset(
+                card.logoAsset,
+                fit: BoxFit.contain,
+                width: double.infinity,
+                height: 76,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -152,75 +207,4 @@ class _DefaultCard {
     required this.logoAsset,
     required this.color,
   });
-}
-
-class _ActionCard extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String subtitle;
-  final VoidCallback onTap;
-
-  const _ActionCard({
-    required this.icon,
-    required this.title,
-    required this.subtitle,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(18),
-      onTap: onTap,
-      child: Container(
-        height: 150,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(18),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.06),
-              blurRadius: 12,
-              offset: const Offset(0, 5),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 28,
-              backgroundColor: const Color(0xFFF8E3EA),
-              child: Icon(
-                icon,
-                color: const Color(0xFFD51B46),
-                size: 28,
-              ),
-            ),
-            const SizedBox(height: 14),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Color(0xFFD51B46),
-                fontSize: 18,
-                fontWeight: FontWeight.w800,
-              ),
-            ),
-            const SizedBox(height: 6),
-            Text(
-              subtitle,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Color(0xFF444446),
-                fontSize: 13,
-                height: 1.2,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
