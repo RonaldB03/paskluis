7ab21b4bf3dc845c629c4ad076f3240ba84e853c
RonaldB03 <ronald.bakker.5@gmail.com>
2026-09-17 12:32:16 +0200

    Connect PasKluis to Supabase

---
+++ lib/core/config/supabase_config.dart
@@ -0,0 +1,5 @@
+abstract final class SupabaseConfig {
+  static const url = 'https://ajldblvvlbvmgejrmhyj.supabase.co';
+  static const publishableKey =
+      'sb_publishable_D22GtKy7nDvnLv7LeBL1SA_1_ZGbN7d';
+}
