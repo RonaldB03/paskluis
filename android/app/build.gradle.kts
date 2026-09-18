957191db80e2cf8a99347222e5ac8a3d2d351782
RonaldB03 <ronald.bakker.5@gmail.com>
2026-09-18 11:44:34 +0200

    Fix Android R8 and iOS export configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,6 +47,10 @@ android {
 
     buildTypes {
         release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
             if (!codemagicKeystorePath.isNullOrBlank()) {
                 signingConfig = signingConfigs.getByName("release")
             }
